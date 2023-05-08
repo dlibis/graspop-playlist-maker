618e94d38f56973f8dddb6542433fbb2e3524109
dlibis <thefonz122@gmail.com>
2023-05-08 18:07:27 +0300

    update env

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,5 @@
-export const scope = process.env.SCOPE!;
-export const redirect_uri = process.env.REDIRECT_URI!;
-export const spotify_client_id = process.env.SPOTIFY_CLIENT_ID!;
+export const scope = process.env.NEXT_PUBLIC_SCOPE!;
+//export const redirect_uri = process.env.REDIRECT_URI!;
+export const spotify_client_id = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID!;
 export const api_url = process.env.NEXT_PUBLIC_API_URL!;
 export const lastfm_api = process.env.NEXT_PUBLIC_LASTFM_API!;
