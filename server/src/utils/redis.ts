152c420d87d1e9763da8fdc0b470ef2996d07cde
dlibis <thefonz122@gmail.com>
2023-05-10 21:37:57 +0300

    fix issue with redis get

--- server/src/utils/redis.ts
+++ server/src/utils/redis.ts
@@ -2,7 +2,11 @@ import { redis_host, redis_port } from '@/constants';
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-  url: `redis://${redis_host}:${redis_port}`,
+  legacyMode: true,
+  socket: {
+    port: redis_port,
+    host: redis_host,
+  },
 });
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
